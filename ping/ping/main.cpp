4b70f747e0095f59d35fdc73280460c10e2065f2
System Administrator <root@hexingyudemacbook-pro.local>
2018-11-22 19:52:42 +0800

    first

---
+++ ping/ping/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ping
+//
+//  Created by System Administrator on 2018/11/22.
+//  Copyright © 2018年 贺星宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
